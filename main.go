a0a86e068a851103e4c4edb05751edf80d89ce0c
YTRei <651617532@qq.com>
2022-02-04 12:39:02 +0800

    two

--- main.go
+++ main.go
@@ -9,11 +9,13 @@ func main(){
 	r := gin.Default()
 	r.LoadHTMLGlob("tmpl/*")
 	r.Static("/static", "./static")
-	r.GET("/index", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.html", nil)
+
+	r.GET("/about", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "about.html", nil)
 	})
-	r.GET("/index-2", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index-2.html", nil)
+
+	r.GET("/blog", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "blog.html", nil)
 	})
 
 	r.GET("/contact", func(c *gin.Context) {
@@ -24,8 +26,19 @@ func main(){
 		c.HTML(http.StatusOK, "course.html", nil)
 	})
 
-	r.GET("/single-course", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "single-course.html", nil)
+	r.GET("/index", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", nil)
+	})
+	r.GET("/index-2", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index-2.html", nil)
+	})
+
+	r.GET("/index-3", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index-3.html", nil)
+	})
+
+	r.GET("/no_event", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "no_event.html", nil)
 	})
 
 	r.GET("/signin", func(c *gin.Context) {
@@ -35,6 +48,27 @@ func main(){
 	r.GET("/signup", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "signup.html", nil)
 	})
+
+	r.GET("/single-blog", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "single-blog.html", nil)
+	})
+
+	r.GET("/single-course", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "single-course.html", nil)
+	})
+
+	r.GET("/single-event", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "single-event.html", nil)
+	})
+
+	r.GET("/single-team", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "single-team.html", nil)
+	})
+
+	r.GET("/team", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "team.html", nil)
+	})
+
 
 	r.Run(":2000")
 }
