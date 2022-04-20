5e086f700313318db02c6169195deb1f82ea4523
YTRei <651617532@qq.com>
2022-04-20 23:04:50 +0800

    auto

--- buildprod.sh
+++ buildprod.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 
 #Build web and other services
-cd D:\study\bysj\api
+cd D:/study/bysj/api
 env GOOS=linux GOARCH=amd64 go build -o ../bin/api
 
-cd D:\study\bysj\scheduler
+cd D:/study/bysj/scheduler
 env GOOS=linux GOARCH=amd64 go build -o ../bin/scheduler
 
-cd D:\study\bysj\streamserver
+cd D:/study/bysj/streamserver
 env GOOS=linux GOARCH=amd64 go build -o ../bin/streamserver
 
-cd D:\study\bysj\web
+cd D:/study/bysj/web
 env GOOS=linux GOARCH=amd64 go build -o ../bin/web
